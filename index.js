4bd8acb5be2f42ff292c3507fec43ef6a93c2d23
mariaklean <44209054+mariaklean@users.noreply.github.com>
2018-11-27 20:39:00 +0000

    javascript for carusel

---
+++ index.js
@@ -0,0 +1,11 @@
+$(window).load(function() {
+  $('.flexslider').flexslider({
+    animation: "slide",
+    animationLoop: false,
+    itemMargin: 5,
+    slideshow: true,
+    slideshowSpeed:4000,
+    animationSpeed: 600
+
+  });
+});
