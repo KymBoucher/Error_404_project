86d4e63b65e6fbc4c4dca607564512014c8b3936
mariaklean <44209054+mariaklean@users.noreply.github.com>
2018-11-28 01:33:50 +0000

    add more details on js

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ $(window).load(function() {
     slideshow: true,
     slideshowSpeed:4000,
     animationSpeed: 600
+    animationSpeed: 600,
+    directionNav: false,
+    controlNav: false,
+    itemWidth: 2000,
 
   });
 });
