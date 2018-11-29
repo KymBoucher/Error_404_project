04f5c5a42f51df0e11749afc22ad5e089d8e0ab1
mariaklean <44209054+mariaklean@users.noreply.github.com>
2018-11-29 23:09:04 +0000

    javascript changes

---
+++ index.js
@@ -0,0 +1,24 @@
+const editBox = $('#codeEdit');
+
+
+var editor = CodeMirror.fromTextArea(editBox[0], {
+    lineNumbers: true, mode: 'html'
+  });
+
+editor.on('change', refresh);
+function refresh() {
+  $('#output').html(editor.getValue())  ;
+}
+
+$(window).load(function() {
+  $('.flexslider').flexslider({
+    animation: "slide",
+    animationLoop: false,
+    itemMargin: 5,
+    slideshow: true,
+    slideshowSpeed:4000,
+    directionNav: false,
+    controlNav: false,
+    itemWidth: 2000
+  });
+});
