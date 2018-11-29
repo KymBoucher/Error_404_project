46f08bbd2a8b67a65661e5d69e9ef29c4ab5a897
mariaklean <44209054+mariaklean@users.noreply.github.com>
2018-11-29 21:36:34 +0000

    carusel
    carusel

--- index.js
+++ index.js
@@ -5,11 +5,8 @@ $(window).load(function() {
     itemMargin: 5,
     slideshow: true,
     slideshowSpeed:4000,
-    animationSpeed: 600
-    animationSpeed: 600,
     directionNav: false,
     controlNav: false,
     itemWidth: 2000,
-
   });
 });
